87a20c24b795179d4a8a99ebdeb18977331e4c09
Manuel Hutter <manuel@hutter.io>
2019-05-04 10:23:50 +0200

    Load pods from the API

--- client/src/components/PodList.js
+++ client/src/components/PodList.js
@@ -1,44 +1,26 @@
 import React, { Component } from 'react'
 import Pod from './Pod';
 
-const pods = [
-  {
-    "name": "nginx-7db9fccd9b-jnljs",
-    "namespace": "podstalk",
-    "node": "minikube",
-    "phase": "Pending",
-    "uid": "a9375ce7-6b78-11e9-9b1f-080027970b01"
-  },
-  {
-    "name": "nginx-7db9fccd9b-zq5zh",
-    "namespace": "podstalk",
-    "node": "minikube",
-    "phase": "Running",
-    "uid": "86b37ea4-6b78-11e9-9b1f-080027970b01"
-  },
-  {
-    "name": "nginx-7db9fccd9b-r4c5k",
-    "namespace": "podstalk",
-    "node": "minikube",
-    "phase": "Terminating",
-    "uid": "a1de83a7-6b78-11e9-9b1f-080027970b01"
-  }
-]
-
 class PodList extends Component {
   constructor(props) {
     super(props);
-    this.state = { pods };
+    this.state = { pods: [] };
   }
 
-  onComponentDidMount() {
-
+  componentDidMount() {
+    fetch('/api/pods')
+      .then(res => res.json())
+      .then(pods => {
+        pods.sort((a, b) => a.name > b.name ? 1 : -1)
+        this.setState({ pods })
+      })
+      .catch(console.error)
   }
 
   render() {
     return (
       <div className="pod_list">
-        <h1>Pods in my Namespace</h1>
+        <h1>Pods in my neighborhood</h1>
         {this.state.pods.map(pod => <Pod key={pod.uid} pod={pod} />)}
       </div>
     )
