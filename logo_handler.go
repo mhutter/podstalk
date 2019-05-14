1f81c4fb81562f609a144ac6c95f00cc5cd747b7
Manuel Hutter <manuel@hutter.io>
2019-05-14 10:10:48 +0200

    Add caching headers to logo

--- logo_handler.go
+++ logo_handler.go
@@ -1,10 +1,23 @@
 package podstalk
 
-import "net/http"
+import (
+	"net/http"
+	"os"
+	"time"
+)
 
-var logo = MustAsset("appuioli.png")
+var (
+	logo = MustAsset("appuioli.png")
+	info os.FileInfo
+)
+
+func init() {
+	info, _ = AssetInfo("appuioli.png")
+}
 
 func LogoHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "image/png")
+	w.Header().Set("Last-Modified", info.ModTime().Format(time.RFC1123))
+	w.Header().Set("Cache-Control", "public, max-age=86400")
 	w.Write(logo)
 }
